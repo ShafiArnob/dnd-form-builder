25839b32ee9e0a3f85d44fab3bfa8ef10b0e5559
ShafiArnob <shafiarnob@gmail.com>
2023-11-21 10:12:10 +0600

    form builder structure

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import { PreviewDialogBtn } from "./PreviewDialogBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import PublishFormBtn from "./PublishFormBtn";
+import Designer from "./Designer";
 
 const FormBuilder = ({ form }: { form: Form }) => {
   return (
     <main className="flex flex-col w-full">
-      <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
+      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
         <h2 className="truncate font-medium">
           <span className="text-muted-foreground mr-2">Form:</span>
           {form.name}
@@ -22,6 +23,9 @@ const FormBuilder = ({ form }: { form: Form }) => {
             </>
           )}
         </div>
+      </nav>
+      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+        <Designer />
       </div>
     </main>
   );
